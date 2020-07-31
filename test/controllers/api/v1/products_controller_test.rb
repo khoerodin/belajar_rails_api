8694527a631561c0839212cb06f48f09ce658002
Khoerodin <khoerodin@live.com>
2020-07-31 16:10:15 +0700

    Adds pagination for the products index action to optimize response

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -5,9 +5,15 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
   end
 
-  test "should show products" do
-    get api_v1_products_url(), as: :json
+  test 'should show products' do
+    get api_v1_products_url, as: :json
     assert_response :success
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    assert_not_nil json_response.dig(:links, :first)
+    assert_not_nil json_response.dig(:links, :last)
+    assert_not_nil json_response.dig(:links, :prev)
+    assert_not_nil json_response.dig(:links, :next)
   end
 
   test "should show product" do
